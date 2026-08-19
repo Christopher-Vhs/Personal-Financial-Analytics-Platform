53f6088d6daf68eab30ff36dfe39690562c1df1a
Christopher-Vhs <christopherwawa395@gmail.com>
2026-08-20 00:02:24 +0200

    Formated the structure of the code in each file

--- frontend/personal-financial-analytics-platform/src/pages/Auth/Login.jsx
+++ frontend/personal-financial-analytics-platform/src/pages/Auth/Login.jsx
@@ -1,38 +1,37 @@
-import React from 'react';
-import AuthLayout from '../../components/layouts/AuthLayout';
-import Input from '../../components/layouts/Inputs/Input';
+import React from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import Input from "../../components/layouts/Inputs/Input";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-//Handle Login Form Submit
-const hadleLogin = async (e) => {}
-
+  //Handle Login Form Submit
+  const hadleLogin = async (e) => {};
 
   return (
     <AuthLayout>
-    <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-    <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
-    <p className="text-xs text-slate-700 mt-[5px] mb-6">
-      Please enter your details to log in to your account.
-    </p> 
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in to your account.
+        </p>
 
-    <form onSubmit={hadleLogin}>
-      <Input
-      value={email}
-      onChange={({target}) => setEmail(target.value)}
-      label="Email Address"
-      placeholder="john@example.com"
-      type="text"
-      />
-    </form>
-    </div>
-   </AuthLayout>
-  )
-}
- 
-export default Login
+        <form onSubmit={hadleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+          />
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
