a090217f17a11fd997c0eb5c877e406d32a5a0b1
Christopher-Vhs <christopherwawa395@gmail.com>
2026-08-22 23:28:34 +0200

    Connected mongoDB and added keys in .env file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -12,10 +13,14 @@ app.use(
     origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 
 app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
