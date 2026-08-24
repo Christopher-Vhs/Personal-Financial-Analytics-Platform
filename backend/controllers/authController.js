09ed85059e280aeace1f0e5bb087515bab232b10
Christopher-Vhs <christopherwawa395@gmail.com>
2026-08-24 21:56:03 +0200

    Created and tested authentication APIs for  get user info and profile image upload

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -68,5 +68,20 @@ exports.loginUser = async (req, res) => {
       .json({ message: "Error logging in user", error: err.message });
   }
 };
+
 // Get User Info
-exports.getUserInfo = async (req, res) => {};
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
+};
---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,36 @@
+const multer = require("multer");
+const path = require("path");
+
+// Configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// File filter
+const fileFilter = (req, file, cb) => {
+  const extension = path.extname(file.originalname).toLowerCase();
+
+  const allowedExtensions = [".jpg", ".jpeg", ".png"];
+
+  if (allowedExtensions.includes(extension)) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error("Only .jpeg, .jpg and .png files are allowed"),
+      false
+    );
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
+
+module.exports = upload;
